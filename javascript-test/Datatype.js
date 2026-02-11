4415c50cac07351c21795bfd3da7f224cac47f3c
Murali-97 <murali97t@gmail.com>
2026-02-11 21:28:08 +0530

    myworks

---
+++ javascript-test/Datatype.js
@@ -0,0 +1,30 @@
+let temp=25.5;
+console.log("Temperature of a city in degrees Celsius:"+temp);
+
+let oderPlaced=true;
+console.log("Whether a customer has placed an order:"+oderPlaced);
+
+let phoneNumber="123-456-7890";
+console.log("Person's phone number: "+phoneNumber);
+
+let  amount=1000.50;
+console.log("Amount of money in a customer's bank account: "+amount);
+
+let email="john.doe@example.com";
+console.log("Person's email address: "+email);
+
+let coordinates="37.7749, -122.4194";
+console.log("Coordinates of a location (latitude, longitude): "+coordinates);
+
+
+let isMarried=false;
+console.log("Person's marital status: "+isMarried);
+
+let occupation="Software Engineer";
+console.log("Person's occupation: "+occupation);
+
+let favouriteColour="Blue";
+console.log("Person's favourite colour: "+favouriteColour);
+
+let currentYear=2023;
+console.log("Current year: "+currentYear);
