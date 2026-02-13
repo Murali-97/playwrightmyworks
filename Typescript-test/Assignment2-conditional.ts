7863e5216a0532bf5898fd3ccf6fee4ce213715d
Murali-97 <murali97t@gmail.com>
2026-02-13 15:32:14 +0530

    Assignment2 conditional

---
+++ Typescript-test/Assignment2-conditional.ts
@@ -0,0 +1,38 @@
+
+
+
+function custmerdetails(customername : String , creditscore : number , income : number , isEmployed : boolean , debittoincomeratio : number){
+
+// customername= customername;
+// creditscore= creditscore;
+// income= income; 
+// isEmployed= isEmployed;
+// debittoincomeratio= debittoincomeratio;
+
+if(creditscore >750){
+        console.log("customer loan is approved");
+    }
+    else if((creditscore > 650 && creditscore <750 )){
+
+        if(income >=50000){
+            console.log("customer loan is considered for approval");
+            if(isEmployed==true){
+                console.log("debttoincomeratio is " + debittoincomeratio);
+                if(debittoincomeratio < 40){
+                    console.log("customer loan is approved");
+                }
+                else{
+                    console.log("customer loan is not approved due to high debt-to-income ratio");
+                }
+        }
+
+            console.log("additional checks are performed for loan approval");
+    }
+    else if(creditscore < 650){
+        console.log("customer loan is not approved");
+      }
+     }
+}
+
+
+custmerdetails("John Doe", 720, 60000, true, 35.0);
--- javascript-test/Hello-world.js
+++ javascript-test/Hello-world.js
@@ -10,3 +10,8 @@ console.log("person details:"+persondetails.age);
 
 let empnames=["murali",23,"vizag"];
 console.log("employee details:"+empnames[0]);
+
+var date = new Date();
+console.log("current date and time:"+date);
+
+console.log("date of birth:"+ date.getFullYear());
